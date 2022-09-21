6b91c45a7b7eb04ead1f94266d20c44f8556e7c3
Leeso <leeso@LeesodeMacBook-Air.local>
2022-09-21 23:53:41 +0800

    restructure

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 "use strict";
 
-const guess = document.querySelector(".guess");
 const score = document.querySelector(".score");
-const secretNumber = document.querySelector(".number");
-const highScore = document.querySelector(".highscore");
+const scoreHigh = document.querySelector(".highscore");
+const secretBlock = document.querySelector(".number");
 const background = document.querySelector("body");
 
 let answer = Math.floor(Math.random() * 20 + 1);
@@ -16,31 +15,28 @@ const resetGame = function () {
   score.textContent = 20;
   background.classList.remove("win");
   hints("Start guessing...");
-  secretNumber.textContent = "?";
-  guess.value = null;
+  secretBlock.textContent = "?";
+  document.querySelector(".guess").value = null;
   answer = Math.floor(Math.random() * 20 + 1);
 };
 
 document.querySelector(".check").addEventListener("click", () => {
-  if (
-    guess.value === "" ||
-    isNaN(Number(guess.value)) ||
-    Number(guess.value) > 20 ||
-    Number(guess.value) < 1
-  ) {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (guess === 0 || guess > 20 || guess < 1) {
     hints("INVALID!!! 🤬");
   } else {
-    if (Number(guess.value) === answer) {
+    if (guess === answer) {
       hints("BINGOOOO!!! 🥳");
-      secretNumber.textContent = answer;
-      highScore.textContent =
-        score.textContent > highScore.textContent
+      secretBlock.textContent = answer;
+      scoreHigh.textContent =
+        score.textContent > scoreHigh.textContent
           ? score.textContent
-          : highScore.textContent;
+          : scoreHigh.textContent;
       background.classList.add("win");
-    } else if (score.textContent > "1" && Number(guess.value) != answer) {
-      score.textContent = Number(score.textContent) - 1;
-      hints(Number(guess.value) > answer ? "Too High 👍🏻" : "Too Low! 👎🏻");
+    } else if (score.textContent > "1" && guess != answer) {
+      score.textContent = score.textContent - 1;
+      hints(guess > answer ? "Too High 👍🏻" : "Too Low! 👎🏻");
     } else {
       score.textContent = 0;
       hints("Sorry! loser! 🤣");
